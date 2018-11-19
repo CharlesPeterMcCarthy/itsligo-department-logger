dd0a13fbd2765448c08bd79873ae6066f5075abc
CharlesPeterMcCarthy <charles@studybundles.com>
2018-11-19 16:55:46 +0000

    Save URLs To Database

--- courses.py
+++ courses.py
@@ -1,5 +1,7 @@
 import os
 import selenium
+import sys
+import pymongo
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -9,6 +11,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import UnexpectedAlertPresentException
+
+try:
+    print("Attempting to connect to MongoDB...")
+    mongo = pymongo.MongoClient("mongodb://localhost:27017/")
+    mongo.server_info()
+except ConnectionFailure:
+    print("Failed to connect to MongoDB. Please run MongoDB first and try again.")
+    exit()
+print("Successfully connected to MongoDB.")
+
 
 urls = []
 
@@ -22,8 +34,8 @@ WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
 
 gatheringURLs = True
 continueOk = True
-deptCount = 1
-courseCount = 0
+deptCount = 16
+courseCount = 30
 
 deptSelect = Select(driver.find_element_by_css_selector('[onchange="FilterStudentSets(swsform)"]'))
 deptSelect.select_by_index(deptCount) # Set the first department to get course count
@@ -36,9 +48,11 @@ while gatheringURLs and courseCount < totalCourses:
     try:
         deptSelect = Select(driver.find_element_by_css_selector('[onchange="FilterStudentSets(swsform)"]'))
         deptSelect.select_by_index(deptCount)
+        currentDept = deptSelect.first_selected_option.text
     except NoSuchElementException:  # Tried to select a department that doesn't exists (all should be finished)
-        print("No more departments")
+        print("No more departments.")
         gatheringURLs = False
+        break
 
     try:
         courseSelect = Select(driver.find_element_by_name('identifier'))
@@ -46,6 +60,10 @@ while gatheringURLs and courseCount < totalCourses:
         if courseCount > 0:
             courseSelect.deselect_by_index(courseCount - 1) # Deselect last course
         courseSelect.select_by_index(courseCount)
+        currentCourse = courseSelect.first_selected_option.text
+
+        sys.stdout.write("Retrieving %s timetable..." %currentCourse)
+        sys.stdout.flush()
     except (NoSuchElementException, UnexpectedAlertPresentException) as e: # Tried to select a course that doesn't exist (move to next dept)
         print("No more courses")
         courseCount = 0
@@ -58,12 +76,23 @@ while gatheringURLs and courseCount < totalCourses:
 
         driver.find_element_by_css_selector('[onclick="getTimetable(swsform, \'student+set\')"]').click() # Click button to view timetable
 
-        urls.append(driver.current_url)
+        urls.append({'dept': currentDept, 'course': currentCourse, 'url': driver.current_url})
         driver.execute_script("window.history.go(-1)") # Go back to timetable lookup
         courseCount+=1
 
+        sys.stdout.write("\rRetrieving %s timetable... Done.\n" %currentCourse)
+        sys.stdout.flush()
     if courseCount == totalCourses:
         courseCount = 0
         deptCount+=1
 
-print(urls)
+if len(urls):
+    print("All timetable URLs collected.")
+    print("Saving URLs to database.")
+    mydb = mongo["test_db"]
+    mycol = mydb["timetables"]
+    mycol.insert_many(urls)
+else:
+    print("No timetable URLs collected.")
+
+print("Finished.")
